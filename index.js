8b850699a513eb42689ef496949397e00af8fa24
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-10-25 02:17:51 +0300

    courses and course path set

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const courses = require('./data/dataSchool.json')
 
 app.get('/', (req, res) => {
     res.send('data scholling is running on local server')
 })
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse);
+
+})
+
 app.listen(port, () => {
     console.log('running on port', port);
 })
